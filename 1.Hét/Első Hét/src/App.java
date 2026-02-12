3953dd5ae95351150ddfff900be15eb3d28d1ad0
kallaigergo <kallai.gergo@student.nye.hu>
2026-02-12 21:10:28 +0100

    1.Hét feladatai

---
+++ 1.Hét/Első Hét/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Nevem: Kállai Gergő");
+        System.out.println("Neptun kódom: RE98AF");
+        System.out.println("Kedvenc Idézetem: Lenni vagy nem lenni, ez itt a kérdés.");
+    }
+}
