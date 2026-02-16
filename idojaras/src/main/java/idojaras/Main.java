3f82f45d5c9e587821c35cb1eb23dba3ded1096f
kallaigergo <kallai.gergo@student.nye.hu>
2026-02-16 19:41:36 +0100

    2.heti feladatok

--- idojaras/src/main/java/idojaras/Main.java
+++ idojaras/src/main/java/idojaras/Main.java
@@ -7,42 +7,60 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     
-    Scanner in = new Scanner(System.in);
+     Scanner in = new Scanner(System.in);
 
-    //1.feladat
-    //Bekérünk egy számot hogy hny fok van
-    System.out.println("Hany fok van ma? ");
-    int idojaras = Integer.parseInt(in.nextLine());
+     //1.feladat
+     System.out.println("Hany fok van ma? ");            //Bekérünk egy számot hogy hny fok van
+     int idojaras = Integer.parseInt(in.nextLine());
 
-    //Kiíratjuk hogy hány fok van
-    System.out.println("Az idojaras " + idojaras + "C°");
-    if (idojaras < 0) {
-        System.out.println("Fagypont alatt van");
-    } else if (idojaras >= 0 && idojaras < 30) {
+     //Kiíratjuk hogy hány fok van
+     System.out.println("Az idojaras " + idojaras + "C°");
+     if (idojaras < 0) {
+         System.out.println("Fagypont alatt van");
+    }  else if (idojaras >= 0 && idojaras < 30) {
         System.out.println("Atalogos");
-    } else if (idojaras > 30) {
+    }  else if (idojaras > 30) {
         System.out.println("Tul meleg van");
     }
 
-    //2.feladat
-    System.out.println("Hany masodperccel halottad a dorgest a villamlás utan?");
-    int masodperc = -1;
-    //Másodperc megadasa, negatív szám esetén újra bekéri egy számot
-    while(masodperc < 0) {
-       masodperc = Integer.parseInt(in.nextLine());
+     //2.feladat
+     System.out.println("Hany masodperccel halottad a dorgest a villamlás utan?");
+     int masodperc = -1; 
+     while(masodperc < 0) {                              
+       masodperc = Integer.parseInt(in.nextLine());     
         if (masodperc < 0) {
             System.out.println("Pozitiv szamot daj meg!");
         }
     }
-    int sebesseg = 300; //m/s
-    int tavolsag = sebesseg * masodperc;
-    System.out.println("A villamlas tavolsaga " + tavolsag + " meter");
-    
+     int sebesseg = 300; //m/s
+     int tavolsag = sebesseg * masodperc;
+        System.out.println("A villamlas tavolsaga " + tavolsag + " meter");
 
+        //3.feladat
+     System.out.println("Hany eves vagy?");
+        int eletkor = 0;
+            while (eletkor < 1 || eletkor > 20) {
+                eletkor = Integer.parseInt(in.nextLine());
+            if (eletkor < 1 || eletkor > 20) {
+            System.out.println("Az intervallumon beluli szamot adj meg!");  // Ha nem 1 és 20 közötti számot adunk meg akkor újra bekéri a számot
+         }
+        }
 
-    
+        // Torta kirajzolasa
+        for ( int i = 0; i < eletkor; i++) {   
+            System.out.printf("* ");
+        }
+            System.out.println();
 
-    
-    
-}
+        for ( int i = 0; i < eletkor; i++) { 
+            System.out.printf("| ");        
+        }
+            System.out.println();
+
+        for ( int i = 0; i < eletkor; i++) {   
+            System.out.printf("- ");
+        }
+            System.out.println();
+        
+    }   
 }
